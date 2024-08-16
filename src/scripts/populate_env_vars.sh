2d53c05fc600b51f103b2874e9f08ab10ca900c7
Justin Burr <20321959+juburr@users.noreply.github.com>
2024-08-16 17:42:59 -0400

    fix: fix digest var failure when image was not yet pushed (#14)
    Co-authored-by: Justin Burr <juburr@users.noreply.github.com>

--- src/scripts/populate_env_vars.sh
+++ src/scripts/populate_env_vars.sh
@@ -26,14 +26,14 @@ set -e
 
 # Read command arguments
 IMAGE=$(circleci env subst "${PARAM_IMAGE}")
-IMAGE_DIGEST_ENV_VAR=$(circleci env subst "${PARAM_IMAGE_DIGEST_ENV_VAR}")
 IMAGE_URI_ENV_VAR=$(circleci env subst "${PARAM_IMAGE_URI_ENV_VAR}")
+IMAGE_URI_DIGEST_ENV_VAR=$(circleci env subst "${PARAM_IMAGE_URI_DIGEST_ENV_VAR}")
 TAG_ENV_VAR=$(circleci env subst "${PARAM_TAG_ENV_VAR}")
 
 # Print arguments for debugging purposes
 echo "Populating environment variables..."
 echo "  IMAGE: ${IMAGE}"
-echo "  IMAGE_DIGEST_ENV_VAR: ${IMAGE_DIGEST_ENV_VAR}"
+echo "  IMAGE_URI_DIGEST_ENV_VAR: ${IMAGE_URI_DIGEST_ENV_VAR}"
 echo "  IMAGE_URI_ENV_VAR: ${IMAGE_URI_ENV_VAR}"
 echo "  TAG_ENV_VAR: ${TAG_ENV_VAR}"
 
@@ -60,44 +60,61 @@ echo "  MOST_SPECIFIC_TAG=${MOST_SPECIFIC_TAG}"
 IMAGE_URI="${IMAGE}:${MOST_SPECIFIC_TAG}"
 echo "  IMAGE_URI=${IMAGE_URI}"
 
-# Compute: IMAGE_DIGEST
+# Compute: IMAGE_DIGEST (only request if you've pushed your image already...)
 IMAGE_DIGEST=""
 if command -v crane 1> /dev/null; then
     echo "  TOOL: crane"
+
     # While docker inspect returns registry/image@sha256:hash, crane simply returns
     # sha256:hash. We need to add the registry/image@ prefix ourselves.
-    DIGEST=$(crane digest "${IMAGE}")
+    DIGEST=$(crane digest "${IMAGE}") || true
     echo "  DIGEST=${IMAGE_URI}"
-    IMAGE_DIGEST="${IMAGE}@${DIGEST}"
-    echo "  IMAGE_DIGEST=${IMAGE_DIGEST}"
+
+    if [[ -n ${DIGEST} ]]; then
+        IMAGE_DIGEST="${IMAGE}@${DIGEST}"
+        echo "  IMAGE_DIGEST=${IMAGE_DIGEST}"
+    else
+        echo "An non-fatal error occurred when trying to compute the image digest."
+        echo "Ensure the image name is correct and that the image has already been pushed to the registry."
+        echo "This command will fail if the image only exists locally."
+        echo "Continuing without setting ${IMAGE_URI_DIGEST_ENV_VAR}..."
+    fi
 elif command -v docker 1> /dev/null; then
     echo "  TOOL: docker"
+
     # When pushing a single image to multiple registries, docker inspect always returns
     # a registry/image@sha256:hash value with the first registry you attempted to use, even if
     # $IMAGE is that of the second registry. Reconstruct the correct value ourselves.
-    DIGEST_WITH_REGISTRY=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE_URI}")
+    DIGEST_WITH_REGISTRY=$(docker inspect --format='{{index .RepoDigests 0}}' "${IMAGE_URI}") || true
     echo "  DIGEST_WITH_REGISTRY=${DIGEST_WITH_REGISTRY}"
-    DIGEST=$(echo "${DIGEST_WITH_REGISTRY}" | cut -d '@' -f 2)
-    echo "  DIGEST=${DIGEST}"
-    IMAGE_DIGEST="${IMAGE}@${DIGEST}"
-    echo "  IMAGE_DIGEST=${IMAGE_DIGEST}"
-elif [[ -n "${IMAGE_DIGEST_ENV_VAR}" ]]; then
-    echo "Requesting ${IMAGE_DIGEST_ENV_VAR} requires that either crane or docker be installed."
-    exit 1
+
+    if [[ -n ${DIGEST_WITH_REGISTRY} ]]; then
+        DIGEST=$(echo "${DIGEST_WITH_REGISTRY}" | cut -d '@' -f 2)
+        echo "  DIGEST=${DIGEST}"
+        IMAGE_DIGEST="${IMAGE}@${DIGEST}"
+        echo "  IMAGE_DIGEST=${IMAGE_DIGEST}"
+    else
+        echo "An non-fatal error occurred when trying to compute the image digest."
+        echo "Ensure the image name is correct and that the image has already been pushed to the registry."
+        echo "This command will fail if the image only exists locally."
+        echo "Continuing without setting ${IMAGE_URI_DIGEST_ENV_VAR}..."
+    fi
+elif [[ -n "${IMAGE_URI_DIGEST_ENV_VAR}" ]]; then
+    echo "An non-fatal error occurred when trying to compute the image digest."
+    echo "Requesting ${IMAGE_URI_DIGEST_ENV_VAR} requires that either crane or docker be installed."
+    echo "Continuing without setting ${IMAGE_URI_DIGEST_ENV_VAR}..."
 fi
 
 # ==========================================
 # Export to .bashrc
 # ==========================================
 echo "Exporting values..."
-BASHRC_COUNT=0
 
 # Adds the most specific tag
 #   e.g., export TAG=3.5.3
 if [[ -n "${TAG_ENV_VAR}" ]]; then
     export "${TAG_ENV_VAR}"="${MOST_SPECIFIC_TAG}"
     echo "export ${TAG_ENV_VAR}=${MOST_SPECIFIC_TAG}" | tee -a "${BASH_ENV}"
-    ((BASHRC_COUNT++))
 fi
 
 # Adds the full image URI to the .bashrc file, using the most specific tag.
@@ -105,19 +122,13 @@ fi
 if [[ -n "${IMAGE_URI_ENV_VAR}" ]]; then
     export "${IMAGE_URI_ENV_VAR}"="${IMAGE_URI}"
     echo "export ${IMAGE_URI_ENV_VAR}=${IMAGE_URI}" | tee -a "${BASH_ENV}"
-    ((BASHRC_COUNT++))
 fi
 
 # Places the image URI and digest in the .bashrc file, often required by tools like Cosign.
 #   e.g., export IMAGE_DIGEST=ghcr.io/org/repo@sha256:3d2b68dd6fa75bd4419533270698b27ab6a482aa2ac5ddb41435fe1fc1bab75c
-if [[ -n "${IMAGE_DIGEST_ENV_VAR}" ]]; then
-    export "${IMAGE_DIGEST_ENV_VAR}"="${IMAGE_DIGEST}"
-    echo "export ${IMAGE_DIGEST_ENV_VAR}=${IMAGE_DIGEST}" | tee -a "${BASH_ENV}"
-    ((BASHRC_COUNT++))
+if [[ -n "${IMAGE_URI_DIGEST_ENV_VAR}" ]]; then
+    export "${IMAGE_URI_DIGEST_ENV_VAR}"="${IMAGE_DIGEST}"
+    echo "export ${IMAGE_URI_DIGEST_ENV_VAR}=${IMAGE_DIGEST}" | tee -a "${BASH_ENV}"
 fi
-
-# Debugging: Display the last ${BASHRC_COUNT} lines from ${BASH_ENV}
-echo "Showing the last ${BASHRC_COUNT} lines from ${BASH_ENV}:"
-tail -n "${BASHRC_COUNT}" "${BASH_ENV}"
 
 printf "Done setting environment variables."
